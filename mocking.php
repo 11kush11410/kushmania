a83ced5c671d848ca13ba4891ac95bde09263798
11kush11410 <66750934+11kush11410@users.noreply.github.com>
2020-06-13 15:59:26 +0530

    Create mocking.php

---
+++ mocking.php
@@ -0,0 +1,16 @@
+!!! Welcome To My World !!!
+
+
+<?php
+
+<html>
+<body>
+
+<h1>Hybrid Multi CLoud</h1>
+
+<img src="" width="42" height="42" style="vertical-align:top"
+
+echo " “See, you not only have to be a good coder to create a system like Linux, you have to be a sneaky bastard too.” (By Linus Torvalds)";
+</body>
+</html>
+?>
