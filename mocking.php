e251b86656fae7672a0a585b129d9eb9a4a9dd69
11kush11410 <66750934+11kush11410@users.noreply.github.com>
2020-06-13 16:05:49 +0530

    Update mocking.php

--- mocking.php
+++ mocking.php
@@ -1,16 +1,16 @@
 !!! Welcome To My World !!!
 
-
+<pre>
 <?php
+echo '
 
+ “See, you not only have to be a good coder to create a system like Linux, you have to be a sneaky bastard too.” (By Linus Torvalds)
+';
+
+echo'
 <html>
-<body>
-
-<h1>Hybrid Multi CLoud</h1>
-
-<img src="" width="42" height="42" style="vertical-align:top"
-
-echo " “See, you not only have to be a good coder to create a system like Linux, you have to be a sneaky bastard too.” (By Linus Torvalds)";
-</body>
+<img src="https://github.com/11kush11410/hihvjhmanshu.git" width="42" height="42" style="vertical-align:top"
 </html>
+';
 ?>
+</pre>
